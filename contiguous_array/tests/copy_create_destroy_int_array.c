2da001f5cc433e962d0d5882054e49835514cd50
WaiTsun-Yeung <williamywt0624@gmail.com>
2024-08-04 01:08:24 +0100

    Improves copy_int_array and copy_create_destroy_int_array to validate same values after copy.

--- contiguous_array/tests/copy_create_destroy_int_array.c
+++ contiguous_array/tests/copy_create_destroy_int_array.c
@@ -6,6 +6,9 @@ int main(){
         struct cds_array array_0 
             = cds_create_array(cds_rand_positive(), sizeof(int));
         struct cds_array array_1 = cds_copy_and_create_array(&array_0);
+        for (int j = 0; j < array_0.data_length; ++j)
+            if (((int*)array_0.data)[j] != ((int*)array_1.data)[j])
+                return 1;
         cds_destroy_array(&array_1);
         cds_destroy_array(&array_0);
     }
--- contiguous_array/tests/copy_int_array.c
+++ contiguous_array/tests/copy_int_array.c
@@ -7,6 +7,9 @@ int main(){
         struct cds_array array_0 = cds_create_array(array_length, sizeof(int));
         struct cds_array array_1 = cds_create_array(array_length, sizeof(int));
         cds_copy_array(&array_1, &array_0);
+        for (int j = 0; j < array_0.data_length; ++j)
+            if (((int*)array_0.data)[j] != ((int*)array_1.data)[j])
+                return 1;
         cds_destroy_array(&array_1);
         cds_destroy_array(&array_0);
     }
