da5df3bcb1af340965e66808ecd4d307c3197e3b
WaiTsun-Yeung <williamywt0624@gmail.com>
2024-08-18 20:31:03 +0100

    Adds a generic cds_destroy_buffer function.

--- utilities/private/utilities.c
+++ utilities/private/utilities.c
@@ -10,3 +10,15 @@ void* cds_malloc_buffer(const size_t buffer_length){
     while (!buffer);
     return buffer;
 }
+
+/// @brief Free the allocated buffer, and replace the pointer to the array with
+///     a null pointer.
+/// @param[in,out] buffer The (pointer to the pointer to the) buffer 
+//      to be freed.
+/// @return A null pointer.
+void* cds_destroy_buffer(void** const buffer){
+    if (!*buffer) return *buffer;
+    free(*buffer);
+    *buffer = (void*)0;
+    return *buffer;
+}
--- utilities/public/utilities.h
+++ utilities/public/utilities.h
@@ -1,3 +1,5 @@
 #include <stddef.h>
 
 void* cds_malloc_buffer(const size_t buffer_length);
+
+void* cds_destroy_buffer(void** const buffer);
