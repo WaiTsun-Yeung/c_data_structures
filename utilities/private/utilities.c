abfe26756fbb25d1bc6c31892f9e60c0400a7c6c
WaiTsun-Yeung <williamywt0624@gmail.com>
2024-08-23 20:25:26 +0100

    Changes buffer_length to bytes in cds_malloc_buffer for readability.

--- utilities/private/utilities.c
+++ utilities/private/utilities.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 /// @brief Heap-allocate a buffer of the specified number of bytes.
-/// @param[in] buffer_length The number of bytes to be allocated.
+/// @param[in] bytes The number of bytes to be allocated.
 /// @return The heap-allocated buffer.
-void* cds_malloc_buffer(const size_t buffer_length){
+void* cds_malloc_buffer(const size_t bytes){
     void* buffer;
-    do buffer = malloc(buffer_length);
+    do buffer = malloc(bytes);
     while (!buffer);
     return buffer;
 }
--- utilities/public/utilities.h
+++ utilities/public/utilities.h
@@ -1,5 +1,5 @@
 #include <stddef.h>
 
-void* cds_malloc_buffer(const size_t buffer_length);
+void* cds_malloc_buffer(const size_t bytes);
 
 void* cds_destroy_buffer(void** const buffer);
