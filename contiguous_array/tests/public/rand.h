84d9b0d3a3bac1e591a716385ae4e64e2ed26e47
WaiTsun-Yeung <williamywt0624@gmail.com>
2024-08-26 11:27:00 +0100

    promotes cds_rand_range to an inline function

--- contiguous_array/tests/public/rand.h
+++ contiguous_array/tests/public/rand.h
@@ -1,2 +1,10 @@
+#include <stdlib.h>
+
 int cds_rand_positive();
-int cds_rand_range(const int min, const int max);
+
+#ifndef CDS_RAND_H
+#define CDS_RAND_H
+    static inline int cds_rand_range(const int min, const int max){
+        return min + rand() / (RAND_MAX / (max - min + 1) + 1);
+    }
+#endif
