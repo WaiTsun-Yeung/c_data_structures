8effa9ca570b063ac8f41bc8d98654ca339e3628
WaiTsun-Yeung <williamywt0624@gmail.com>
2024-09-15 22:02:33 +0100

    Improves test of copy_singly_linked_list_node.c

--- singly_linked_list/tests/copy_singly_linked_list_node.c
+++ singly_linked_list/tests/copy_singly_linked_list_node.c
@@ -12,6 +12,14 @@ int main() {
                 sizeof(unsigned long long), alignof(unsigned long long)
             );
         cds_copy_singly_linked_list_node(&node_0, node_1);
+        if (
+            *(unsigned long long*)node_0->data 
+                != *(unsigned long long*)node_1->data
+        ){
+            cds_destroy_free_singly_linked_list_node(&node_1);
+            cds_destroy_free_singly_linked_list_node(&node_0);
+            return 1;
+        }
         cds_destroy_free_singly_linked_list_node(&node_1);
         cds_destroy_free_singly_linked_list_node(&node_0);
     }
