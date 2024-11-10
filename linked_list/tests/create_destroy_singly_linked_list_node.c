eb8e3d6bbde384a5b555f67a3f94cf82dfae46b5
WaiTsun-Yeung <williamywt0624@gmail.com>
2024-11-10 19:50:19 +0000

    Adds passed changed test create_destroy_singly_linked_list_node

--- linked_list/tests/create_destroy_singly_linked_list_node.c
+++ linked_list/tests/create_destroy_singly_linked_list_node.c
@@ -1,13 +1,19 @@
 #include <stdalign.h>
 
+#include "status.h"
+
 #include "singly_linked_list_type.h"
 #include "singly_linked_list.h"
 
 int main() {
     for (size_t i = 0; i < 1000000; ++i){
+        enum cds_status return_state;
         struct cds_singly_linked_list_node* node 
-            = cds_create_singly_linked_list_node(sizeof(int), alignof(int));
-        cds_destroy_free_singly_linked_list_node(&node);
+            = cds_create_singly_linked_list_node(
+                sizeof(int), alignof(int), &return_state
+            );
+        if (return_state) return return_state;
+        cds_destroy_free_singly_linked_list_node(&node, &return_state);
     }
     return 0;
 }
